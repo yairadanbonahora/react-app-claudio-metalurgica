f28e614e2a082a7e13d543d11b9905de86c78527
yairadanbonahora <bonahorayair@gmail.com>
2022-03-23 18:40:27 -0300

    Contador con botón y stock

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,22 +1,34 @@
 import React, {useState} from 'react';
 
 function ItemCount () {
-    const [initial, addToCart] = useState(0);
+    const [initial, contador] = useState(0);
+    const [stock, setStock] = useState(5);
     
-    const deleteOne = () => {
-        if (initial>=1) {
-            addToCart (initial-1);
-        }
+    const click = (type) => {
+        if (type === "addToCart") {
+            if (stock >= 1) {
+                contador (initial +1);
+                setStock (stock -1); 
+            }
+           
+        } else {
+            if (initial>=1) {
+                contador (initial-1);
+                setStock (stock +1);
+            }
+        } 
+
     }
-    
+        
     return (
         <div className="card-producto">
             <h3>Balancín</h3>
             <div>
                 <h6>Cantidad: {initial}</h6>
-                <button onClick={()=> addToCart (initial +1)}>Agregar al carrito</button>
-                <button onClick={()=> deleteOne ()}>Quitar una unidad</button>
+                <button onClick={()=> click ("addToCart")}>Agregar al carrito</button>
+                <button onClick={()=> click ("deleteOne")}>Quitar una unidad</button>
             </div>
+            <h9>Stock: {stock}</h9>
         </div>
     )
 };
